b8d4b2e5bfc533e43ba055bdf1cba411ad4eb8d9
pisesrathmony <pisesrathmony.n@api-school.com>
2025-06-04 08:25:09 +0700

    Create Art.py

---
+++ Art.py
@@ -0,0 +1,34 @@
+import turtle
+t = turtle. Turtle()
+t. speed (5)t. fillcolor ("yellow")
+t.begin_ fill()
+t. circle ( 100)
+t.end_ fill
+t. right (90)
+t. forward (200)
+10 t. left (60)
+11 t. forward (100)
+12 t. right (60)
+13 t. forward (100)
+14 t. penup(
+15 t. left (90)
+16
+t.forward(50)
+17
+t. pendown ()
+18
+t. penup (
+19
+t. goto(-40, 120)
+20 t. fillcolor ("black")
+21 t.begin_ fill()
+22 t.circle (25)
+23
+t.end_ fill()
+24
+t. pendown ()
+25
+t・penup（）
+26 t. goto (40,120)
+27 t. fillcolor ("black")
+28 t.begin_ fill()
