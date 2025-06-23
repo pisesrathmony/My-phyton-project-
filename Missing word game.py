e358c06d8769eb1a9d656c0e1bf2a456733ea95f
pisesrathmony <pisesrathmony.n@api-school.com>
2025-06-23 08:57:24 +0700

    Create Missing word game.py

---
+++ Missing word game.py
@@ -0,0 +1,23 @@
+import random
+import time
+Choices = ["sad","play","blue"]
+print("Welcome to the missing word game")
+
+def question():
+    answer = input("\nWhich is the missing word?\nsad\nplay\nblue\n")
+    return answer
+    
+QuestionOne = "Sam likes to _______ games."
+print(QuestionOne)
+answer = question()
+if answer == Choices[1]:
+    print("Very good!")
+else:
+    print("Wrong!")
+time.sleep(2)
+
+QuestionTwo = "\nHe was very ______today."
+print(QuestionTwo)
+answer = question()
+if answer == Choices[0]:
+    print("Very good!")
