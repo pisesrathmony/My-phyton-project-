cf5a747573711108d1c8b4b781f1411bc6bcef1d
pisesrathmony <pisesrathmony.n@api-school.com>
2025-07-09 08:40:58 +0700

    Create Flow chart.py

---
+++ Flow chart.py
@@ -0,0 +1,11 @@
+answer = input("Are you hungry?")
+if answer == "no":
+    print("Drink water")
+else:
+    answer = input("sweet or salty?")
+if answer == "sweet":
+    print("Okay try Cookies,Icecream orFruits")
+else:
+    answer = input("Okay try Salad,soup,burger or pasta")
+    
+    
